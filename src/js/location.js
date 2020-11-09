42700f3e6c55bb8a9a8ce7283c6f683281cd5449
waitsunny-li <750791847@qq.com>
2020-11-09 09:50:21 +0800

    完善通过ip地址获取定位

--- src/js/location.js
+++ src/js/location.js
@@ -11,36 +11,38 @@ class MyLocation {
     }
   }
 
-  init(success,error) {
+  init(success, error) {
     let self = this;
     let getmylocation = new qq.maps.Geolocation(self.keyid, "myapp");
-      //获取位置信息
-      getmylocation.getLocation(success, error, self.options);
+    //获取位置信息
+    getmylocation.getLocation(success, error, self.options);
 
   }
 
   // 通过ip地址来获取位置，适合pc端
-  getLoaction() {
+  getLoaction(cb) {
     let key = this.keyid
-    return new Promise((resolve, reject) => {
-      $.ajax({
-        url: `https://apis.map.qq.com/ws/location/v1/ip?output=jsonp&key=${key}`,
-        dataType: "jsonp",
-        jsonp: "callback",
-        jsonpCallback: 'getLocat',
-        type: 'get',
-        success: function (res) {
-          resolve(res)
-        },
-        error: function (error) {
-          console.log(error);
-        }
-      })
+    $.ajax({
+      url: `https://apis.map.qq.com/ws/location/v1/ip?output=jsonp&key=${key}`,
+      dataType: "jsonp",
+      jsonp: "callback",
+      jsonpCallback: 'getLocat',
+      type: 'get',
+      success: function (res) {
+        cb(res)
+      },
+      error: function (error) {
+        console.log(error);
+      }
     })
   }
 
   // 通过经纬度转化为详细地址
-  latLngTransAddress({lat, lng, get_poi}, cb) {
+  latLngTransAddress({
+    lat,
+    lng,
+    get_poi
+  }, cb) {
     let key = this.keyid
     let get_poi_id = get_poi ? get_poi : 0
     $.ajax({
